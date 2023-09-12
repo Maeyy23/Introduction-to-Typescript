2236ed71adf90ccd94f118510c1c27edb8287714
Maeyy23 <msmabel23@gmail.com>
2023-09-12 12:34:53 +0100

    Understanding searching algorithms

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  * it returns an even number as true
  * and an odd number as false */
 function checkEven(number) {
-    if (number % 2 == 0) {
+    if (number % 2 === 0) {
         return true;
     }
     else
@@ -27,3 +27,57 @@ function checkPalindrome(word) {
 }
 ;
 console.log(checkPalindrome("Madam"));
+/**
+ * write a function in typescript that takes in two parameters
+ * and converts them to degree celsius and farenheit
+ */
+function convertTemperature(temp, unit) {
+    if (unit === "fahrenheit") {
+        return (temp - 32) / 1.8;
+    }
+    else {
+        return temp * 1.8 + 32;
+    }
+}
+;
+// console.log(convertTemperature(32,"fahrenheit"));
+// console.log(convertTemperature(32,"celcius"));
+// console.log(convertTemperature(32,"fahrenheit"));
+// console.log(convertTemperature(32,"celcius"));
+/**
+ * Implement Linear and Binary Search Algorithms using typescript
+ */
+/* Linear Search algorithm is a search method that iterates over the items in an array until the targetted item is found or not found.
+*/
+function linearSearch(array, target) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return i;
+        }
+        console.log(" ".concat(target, " not found at ").concat(i));
+    }
+    return -1;
+}
+console.log(linearSearch(["Apple", "Pawpaw", "Mango", "Avocado"], "Orange"));
+// This returns the index [numerical position] of the item
+/**
+ * Binary Search algorithm works with sorted arrays. It splits the array into half. The targetted item range and the outliers.
+ * this is the most efficient way to iterate over a large amount of data in an array
+ */
+function binarySearch(array, target) {
+    var low = 0;
+    var high = array.length - 1;
+    while (low <= high) {
+        var mid = Math.floor((low + high) / 2);
+        if (array[mid] === target) {
+            return mid;
+        }
+        else if (array[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
--- test.ts
+++ test.ts
@@ -39,7 +39,47 @@ function convertTemperature(temp: number, unit: string): number {
     }
 };
 
-console.log(convertTemperature(32,"fahrenheit"));
-console.log(convertTemperature(32,"celcius"));
-console.log(convertTemperature(32,"fahrenheit"));
-console.log(convertTemperature(32,"celcius"));
+// console.log(convertTemperature(32,"fahrenheit"));
+// console.log(convertTemperature(32,"celcius"));
+// console.log(convertTemperature(32,"fahrenheit"));
+// console.log(convertTemperature(32,"celcius"));
+
+/**
+ * Implement Linear and Binary Search Algorithms using typescript
+ */
+
+/* Linear Search algorithm is a search method that iterates over the items in an array until the targetted item is found or not found. 
+*/
+function linearSearch(array: string[], target: string): number {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }console.log(` ${target} not found at ${i}`)
+  }
+  return -1;
+}
+
+console.log(linearSearch(["Apple", "Pawpaw", "Mango", "Avocado"], "Orange"))
+// This returns the index [numerical position] of the item
+
+/**
+ * Binary Search algorithm works with sorted arrays. It splits the array into half. The targetted item range and the outliers.
+ * this is the most efficient way to iterate over a large amount of data in an array
+ */
+function binarySearch(array: number[], target: number): number {
+  let low = 0;
+  let high = array.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (array[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
